38189363fb1f2c3e96a76cde3c736aad95355598
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-06-05 19:11:23 +0100

    update

--- src/arrays.js
+++ src/arrays.js
@@ -1,48 +1,55 @@
 const getNthElement = (index, array) => {
   // your code here
-  for (let i = 0; i < array.length; i++) {
-    if (i != '') {
-      return array([index]).toString();
-    } else { 
-      i = 0;
-    }
-  
-
-
+  if (index > array.length -1) {
+    return array[index % array.length]
+  } else {
+    return array[index];
+  }
 };
 
 const arrayToCSVString = (array) => {
   // your code here
-  
+  return array.join(",");
 };
 
 const csvStringToArray = (string) => {
   // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
   // your code here
+  array.push(element);
 
 };
 
 const addToArray2 = (element, array) => {
   // your code here
+  return array.concat(element);
 };
 
 const removeNthElement = (index, array) => {
   // your code here
+  return array.splice(index, 1);
 };
 
 const numbersToStrings = (numbers) => {
   // your code here
+  return numbers.map(String);
 };
 
 const uppercaseWordsInArray = (strings) => {
   // your code here
+  return strings.map(string => string.toUpperCase());
+
 };
 
 const reverseWordsInArray = (strings) => {
   // your code here
+  function revStr(strings) {
+    return strings.split("").reverse().join("")
+  } 
+  revStr(strings);
 };
 
 const onlyEven = (numbers) => {
