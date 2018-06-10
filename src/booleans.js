dd31df6e85bca6fe0ccff33baf9e33e137905edd
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-06-10 23:34:51 +0100

    formatted

--- src/booleans.js
+++ src/booleans.js
@@ -1,136 +1,136 @@
 const negate = (a) => {
   // your code here
-  if (a) {
-    return false;
-  } else {
-    return true;
-  }
+    if (a) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 const both = (a, b) => {
   // your code here
-  if (a && b) {
-    return true;
-  } else {
-    return false;
+    if (a && b) {
+        return true;
+    } else {
+        return false;
   }
 };
 
 const either = (a, b) => {
   // your code here
-  if (a || b) {
-    return true;
-  } else {
-    return false;
-  }
+    if (a || b) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const none = (a, b) => {
   // your code here
-  if (a, b == 1 || a > b || a < b) {
-    return false; 
-  } else {
-    return true;
-  }
+    if (a, b == 1 || a > b || a < b) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 const one = (a, b) => {
   // your code here
-  if (a === b) {
-    return false;
-  } else {
-    return true;
-  }
+    if (a === b) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 const truthiness = (a) => {
   // your code here
-  if (a) {
-    return true;
-  } else {
-    return false;
-  }
+    if (a) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const isEqual = (a, b) => {
   // your code here
-  if (a == b) {
-    return true;
-  } else {
-    return false;
-  }
+    if (a == b) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const isGreaterThan = (a, b) => {
   // your code here
-  if (a <= b) {
-    return false;
-  } else {
-    return true;
-  }
+    if (a <= b) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 const isLessThanOrEqualTo = (a, b) => {
   // your code here
-  if (a <= b) {
-    return true;
-  } else {
-    return false;
-  }
+    if (a <= b) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const isOdd = (a) => {
   // your code here
-  if (a % 2 > 0) {
-    return true;
-  } else {
-    return false;
-  }
+    if (a % 2 > 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const isEven = (a) => {
   // your code here
-  if (a % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+    if (a % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const isSquare = (a) => {
   // your code here
-  if (Math.sqrt(a) * Math.sqrt(a) === a) {
-    return true;
-  } else {
-    return false;
-  }
+    if (Math.sqrt(a) * Math.sqrt(a) === a) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const startsWith = (char, string) => {
   // your code here
-  if (string.charAt(0) === char) {
-    return true;
-  } else {
-    return false;
-  }
+    if (string.charAt(0) === char) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const containsVowels = (string) => {
   // your code here
-  if (string.match(/[aeiouAEIOU]/)) {
-    return true;
-  } else {
-    return false;
+    if (string.match(/[aeiouAEIOU]/)) {
+        return true;
+    } else {
+        return false;
     }
 };
 
 const isLowerCase = (string) => {
   // your code here
-  if (string === string.toLowerCase()) {
-    return true;
-  } else {
-    return false;
-  }
+    if (string === string.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 module.exports = {
--- src/objects.js
+++ src/objects.js
@@ -1,89 +1,79 @@
 const createPerson = (name, age) => {
-  // your code here
-  /*if (name === 'Fred') {
-  const fred = {
-    name: 'Fred',
-    age: 79,
-  }
-  return fred;
-} else {
-  const dilys = {
-    name: 'Dilys',
-    age: 50,
-  }
-  return dilys;
-}*/
-const person = {
-    name: name,
-    age: age,
-  }
-return person;
+
+    const person = {
+        name: name,
+        age: age,
+    }
+    return person;
 };
 
 const getName = (object) => {
   // your code here
-  return object.name;
+    return object.name;
 };
 
 const getProperty = (property, object) => {
   // your code here
-  return object.age;
+    return object.age;
 };
 
 const hasProperty = (property, object) => {
   // your code here
-  if (property in object) {
-    return true;
-  } else {
-    return false;
-  }
+    if (property in object) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const isOver65 = (person) => {
   // your code here
-  if (person.age > 65) {
-    return true;
-  } else {
-    return false;
+    if (person.age > 65) {
+        return true;
+    } else {
+        return false;
   }
 
 };
 
 const getAges = (people) => {
   // your code here
-  let ages =[];
-  for (let age of people) {
-    ages.push(age.age);
-  }
-  return ages;
+    let ages =[];
+        for (let age of people) {
+            ages.push(age.age);
+            }
+        return ages;
 };
 
 const findByName = (name, people) => {
   // your code here
- let result = people.filter(person => name === person.name);
- let resulto = result.reduce(function(obj,item) {
-   obj[item.key] = item.value;
-   return obj;
+    let result = people.filter(person => name === person.name);
+    let resulto = result.reduce(function(obj,item) {
+        obj[item.key] = item.value;
+            return obj;
  });
- return resulto;
+        return resulto;
 };
 
 const findHondas = (cars) => {
   // your code here
-  let result = cars.filter( cars => cars.manufacturer === 'Honda');
-  return result;
+    let result = cars.filter( cars => cars.manufacturer === 'Honda');
+        return result;
  };
 
 const averageAge = (people) => {
   // your code here
-  let ages =[];
-  for (let age of people) {
-    ages.push(age.age);
-  }
-  let sum = ages.reduce(function(a, b ) { 
-    return a + b; });
-    let avg = sum / ages.length;
-  return avg;
+    let ages =[];
+
+        for (let age of people) {
+            ages.push(age.age);
+            }
+
+        let sum = ages.reduce(function(a, b ) {
+            return a + b; });
+
+        let avg = sum / ages.length;
+            return avg;
 
 };
 
